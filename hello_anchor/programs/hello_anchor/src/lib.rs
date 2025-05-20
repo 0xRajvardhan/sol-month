5e51ea7e7ef5ee1b7330092d2e5eb9d53735f380
CarlGustav <singhrajvardhan2003@gmail.com>
2025-05-20 21:13:58 +0530

    day12 anchor

--- hello_anchor/programs/hello_anchor/src/lib.rs
+++ hello_anchor/programs/hello_anchor/src/lib.rs
@@ -5,11 +5,36 @@ declare_id!("3nantEanKHuNfXSY3uCXEPJsQqJDaSjbLEKZjWLtHeG7");
 #[program]
 pub mod hello_anchor {
     use super::*;
-    pub fn say_hello(_ctx: Context<SayHello>) -> Result<()> {
-        msg!("Hello, Anchor World!");
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.data = "".to_string();
+        Ok(())
+    }
+    pub fn update(ctx: Context<Update>, message: String) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.data = message;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct SayHello {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 280)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+#[account]
+
+pub struct BaseAccount {
+    pub data: String,
+}
--- hello_anchor/tests/hello_anchor.ts
+++ hello_anchor/tests/hello_anchor.ts
@@ -1,11 +1,30 @@
 const anchor = require('@coral-xyz/anchor');
+const { SystemProgram } = anchor.web3;
 
 describe('hello_anchor', () => {
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
   const program = anchor.workspace.HelloAnchor;
 
-  it('Says hello!', async () => {
-    const tx = await program.methods.sayHello().rpc();
-    console.log("Transaction signature:", tx);
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  it('Initializes the account', async () => {
+    await program.methods.initialize().accounts({
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    }).signers([baseAccount]).rpc();
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Initialized with:', account.data);
+  });
+
+  it('Updates the message', async () => {
+    await program.methods.update("Solana is fast!").accounts({
+      baseAccount: baseAccount.publicKey,
+    }).rpc();
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Updated message:', account.data);
   });
 });
